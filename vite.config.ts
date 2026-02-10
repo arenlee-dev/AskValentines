bda37eb4ceae94ae14bba41cbcb6ab2b4255cd28
Aren Dosser UNSW_WSL <z5591284@ad.unsw.edu.au>
2026-02-10 18:51:55 +1100

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	base: "/AskValentines/",
+  plugins: [react(), tailwindcss()],
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  base: "./",
 });
