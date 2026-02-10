3f0fd99a5aa2b48e8da0a68c55241f0621cb109d
Aren Dosser UNSW_WSL <z5591284@ad.unsw.edu.au>
2026-02-10 21:36:21 +1100

    fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import path from "path";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-  base: "./",
+  base: "/AskValentines/",
 });
